5179008d6bff77201ac6288de11419725b49e555
Bot-toctoc <70813661+henri5435345@users.noreply.github.com>
2024-10-10 20:31:17 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+﻿document.getElementById('playerForm').addEventListener('submit', async function (event) {
+    event.preventDefault(); // Empêche le rechargement de la page
+    const playerName = document.getElementById('playerName').value;
+    const response = await fetch(`https://localhost:7120/api/player/${playerName}`);
+
+    if (response.ok) {
+        const data = await response.json();
+        displayPlayerInfo(data);
+    } else {
+        document.getElementById('result').innerText = "Erreur lors de la récupération des données.";
+    }
+});
+
+function displayPlayerInfo(data) {
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = ''; // Efface les résultats précédents
+
+    const playerInfo = document.createElement('div');
+    playerInfo.innerHTML = `<h2>Informations du Joueur</h2>
+                            <pre>${JSON.stringify(data.playerInfo, null, 2)}</pre>`;
+    resultDiv.appendChild(playerInfo);
+
+    const friendsList = document.createElement('div');
+    friendsList.innerHTML = `<h2>Liste des Amis</h2>
+                            <pre>${JSON.stringify(data.friendsList, null, 2)}</pre>`;
+    resultDiv.appendChild(friendsList);
+
+    const playerJob = document.createElement('div');
+    playerJob.innerHTML = `<h2>Emplois</h2>
+                            <pre>${JSON.stringify(data.playerJob, null, 2)}</pre>`;
+    resultDiv.appendChild(playerJob);
+
+    const playerClicker = document.createElement('div');
+    playerClicker.innerHTML = `<h2>Clicker</h2>
+                               <pre>${JSON.stringify(data.playerClicker, null, 2)}</pre>`;
+    resultDiv.appendChild(playerClicker);
+}
